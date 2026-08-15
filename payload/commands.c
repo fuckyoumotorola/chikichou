b2e040a85c7aa7dd365ab008fe1822fe2df4cbbf
Tiziano Tomás Luzi Ramos <tizianoluziramos@gmail.com>
2026-08-15 08:40:19 -0700

    Refactor cmd_help command

--- payload/commands.c
+++ payload/commands.c
@@ -27,58 +27,68 @@ void cmd_download(const char *arg, void *data, unsigned sz) {
 
 void cmd_help(const char *arg, void *data, unsigned sz) {
   fastboot_info("");
-  fastboot_info("Available commands:");
+  fastboot_info("Available stock commands:");
   fastboot_info("");
-  fastboot_info("flash:");
+
+  fastboot_info("boot:");
+  fastboot_info("continue:");
   fastboot_info("download:");
   fastboot_info("erase:");
+  fastboot_info("flash:");
   fastboot_info("getvar:");
   fastboot_info("reboot:");
   fastboot_info("reboot-bootloader:");
   fastboot_info("reboot-recovery:");
-  fastboot_info("continue:");
   fastboot_info("set-active:");
-  fastboot_info("boot:");
+
   fastboot_info("");
-  fastboot_info("oem help");
-  fastboot_info("oem is-partition-protected <partition>");
-  fastboot_info("oem lks");
-  fastboot_info("oem key");
-  fastboot_info("oem p2u");
-  fastboot_info("oem dump_pllk_log");
-  fastboot_info("oem off-mode-charge");
-  fastboot_info("oem printk-ratelimit");
-  fastboot_info("oem get_socid");
-  fastboot_info("oem scp_status <0|1>");
-  fastboot_info("oem scp_log_thru_ap_uart");
-  fastboot_info("oem usb2jtag <0|1>");
-  fastboot_info("oem ultraflash");
-  fastboot_info("oem ultraflash_en <0|1>");
-  fastboot_info("oem keepalive");
+  fastboot_info("Available stock OEM commands:");
+  fastboot_info("");
+
+  fastboot_info("oem batterysn get");
+  fastboot_info("oem carrier get");
   fastboot_info("oem cid");
   fastboot_info("oem cid digest");
-  fastboot_info("oem md5");
-  fastboot_info("oem sha256");
-  fastboot_info("oem flashinfo");
-  fastboot_info("oem fac get");
-  fastboot_info("oem config fac");
-  fastboot_info("oem batterysn get");
   fastboot_info("oem config batterysn");
-  fastboot_info("oem carrier get");
   fastboot_info("oem config carrier <string>");
-  fastboot_info("oem dualsim get");
   fastboot_info("oem config dualsim <0|1>");
-  fastboot_info("oem wallpaper get");
+  fastboot_info("oem config fac");
   fastboot_info("oem config wallpaper <number>");
-  fastboot_info("oem set_active");
-  fastboot_info("oem continue");
-  fastboot_info("oem shutdown");
+  fastboot_info("oem dualsim get");
+  fastboot_info("oem dump_pllk_log");
   fastboot_info("oem efuse enable");
-  fastboot_info("oem ssm enable-thinkshield");
-  fastboot_info("oem ssm enable-zerotouch");
+  fastboot_info("oem fac get");
+  fastboot_info("oem flashinfo");
+  fastboot_info("oem get_socid");
+  fastboot_info("oem keepalive");
+  fastboot_info("oem key");
+  fastboot_info("oem lks");
+  fastboot_info("oem md5");
+  fastboot_info("oem off-mode-charge");
+  fastboot_info("oem p2u");
+  fastboot_info("oem printk-ratelimit");
+  fastboot_info("oem scp_log_thru_ap_uart");
+  fastboot_info("oem scp_status <0|1>");
+  fastboot_info("oem set_active");
+  fastboot_info("oem sha256");
+  fastboot_info("oem shutdown");
   fastboot_info("oem ssm disable-thinkshield");
   fastboot_info("oem ssm disable-zerotouch");
+  fastboot_info("oem ultraflash");
+  fastboot_info("oem ultraflash_en <0|1>");
+  fastboot_info("oem usb2jtag <0|1>");
+  fastboot_info("oem wallpaper get");
+
+  fastboot_info("");
+  fastboot_info("Available custom commands:");
+  fastboot_info("");
+
   fastboot_info("oem download <size_in_hex>");
+  fastboot_info("oem help");
+  fastboot_info("oem is-partition-protected <partition>");
+  fastboot_info("oem ssm enable-thinkshield");
+  fastboot_info("oem ssm enable-zerotouch");
+
   fastboot_okay("");
 }
 
